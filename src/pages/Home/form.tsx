9e0e852fe2dd689af4c9aaee5c6c4262cfb8bc54
Dominik Mielcarek <work@mielcarek.dev>
2024-10-12 03:40:47 +0200

    feat: added wallet address validation

--- src/pages/Home/form.tsx
+++ src/pages/Home/form.tsx
@@ -1,12 +1,30 @@
 import { useState } from "react";
-import { Button, Flex, FormControl, Input } from "@chakra-ui/react";
+import {
+  Button,
+  Flex,
+  FormControl,
+  FormErrorMessage,
+  Input,
+} from "@chakra-ui/react";
+
+function isWalletAddresValid(address: string): boolean {
+  return /^0x[0-9a-f]{40}$/.test(address);
+}
 
 export default function Form({ onSubmit }) {
   const [walletAddress, setWalletAddress] = useState("");
+  const [isValid, setIsValid] = useState(true);
 
   const onFormSubmit = async (e: Event) => {
     e.preventDefault();
 
+    if (!isWalletAddresValid(walletAddress)) {
+      setIsValid(false);
+
+      return;
+    }
+
+    setIsValid(true);
     onSubmit?.(walletAddress);
   };
 
@@ -16,7 +34,7 @@ export default function Form({ onSubmit }) {
 
   return (
     <Flex direction="column" gap="1rem">
-      <FormControl>
+      <FormControl isInvalid={!isValid}>
         <Input
           type="text"
           size="lg"
@@ -24,6 +42,10 @@ export default function Form({ onSubmit }) {
           value={walletAddress}
           onInput={onInputChange}
         ></Input>
+        <FormErrorMessage>
+          Wallet address should consist of "0x" prefix and 40 hexadecimal
+          characters
+        </FormErrorMessage>
       </FormControl>
       <Button colorScheme="teal" size="lg" onClick={onFormSubmit} type="submit">
         Check balance
