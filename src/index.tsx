37f01636fa4baad7fa7092f2613b8e88f6ed7594
Dominik Mielcarek <work@mielcarek.dev>
2024-10-13 01:36:28 +0200

    refactor: removed not used anymore style.css file

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import { render } from "preact";
 import { LocationProvider } from "preact-iso";
-// import "./style.css";
 import AppRouter from "./router";
 import { ChakraProvider } from "@chakra-ui/react";
 
